054f455184d1c9c80737fe82546cc7f095373b3e
GoEun <curruptedog@gmail.com>
2021-06-24 17:12:31 +0900

    backend/join controller

--- src/main/java/Shoetudio/spring/mvc/controller/JoinController.java
+++ src/main/java/Shoetudio/spring/mvc/controller/JoinController.java
@@ -1,24 +1,47 @@
 package Shoetudio.spring.mvc.controller;
 
+import Shoetudio.spring.mvc.service.ArtistService;
+import Shoetudio.spring.mvc.service.UserService;
+import Shoetudio.spring.mvc.vo.Artist;
+import Shoetudio.spring.mvc.vo.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class JoinController {
 
+    private UserService usrv;
+    private ArtistService asrv;
+
+    @Autowired
+    public JoinController (UserService usrv, ArtistService asrv) {
+        this.usrv = usrv;
+        this.asrv = asrv;
+    }
+
     @GetMapping("/join/agree")
     public String agree() {
-        return "agree";
+        return "join/agree.tiles";
     }
 
     @GetMapping("/join/joinme")
     public String joinme() {
-        return "joinme";
+        return "join/joinme.tiles";
     }
 
-    @GetMapping("/join/joinok")
-    public String joinok() {
-        return "joinok";
+    @PostMapping("/join/joinok")
+    public String ujoinok(User u, HttpServletRequest req) {
+
+        System.out.println(u.getUtype());
+
+        usrv.newUser(u);
+
+        return "join/joinok.tiles";
     }
+
 
 }
